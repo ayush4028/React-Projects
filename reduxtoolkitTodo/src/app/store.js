02e2a0a18f59cfa89c4a2f12711ed486dc9bb484
Ayush <ayushsingh00030@gmail.com>
2023-12-29 15:20:03 +0530

    Todo List website - Here State management is done using REDUX toolkit

---
+++ reduxtoolkitTodo/src/app/store.js
@@ -0,0 +1,9 @@
+// configure store 
+import {configureStore} from '@reduxjs/toolkit'
+import todoReducer from '../features/todo/todoSlice'
+
+export const store = configureStore({
+    reducer: todoReducer
+});
+
+
